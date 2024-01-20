1d17159a9a4d480a0cf99a7977187546c2dcf530
David Allen <spondicious@me.com>
2024-01-20 12:24:11 +0100

    First commit

---
+++ a_core/urls.py
@@ -0,0 +1,12 @@
+"""
+URL configuration for a_core project.
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
+    path("", include("a_plot.urls")),
+]
